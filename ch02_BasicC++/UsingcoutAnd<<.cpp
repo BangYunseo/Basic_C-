500d9e69baa232fa088db6435faae1061eda6866
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2023-12-19 21:26:44 +0900

    Create UsingcoutAnd<<.cpp

---
+++ ch02_BasicC++/UsingcoutAnd<<.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+double area(int r){
+  return 3.14 * r * r;
+}
+// 원의 면적을 구하는 함수
+
+int main(){
+  int n = 3;
+  char c = '#';
+  std::cout << c << 5.5 << '-' << n << "hello" << true << std::endl;
+  std::cout << "n + 5 = " << n + 5 << '\n';
+  std::cout << "면적은 << area(n);
+  // 함수 area()의 리턴 값 출력
+}
+
+// 출력 예시
+// #5.5-3hello1
+// n + 5 = 8
+// 면적은 28.26
