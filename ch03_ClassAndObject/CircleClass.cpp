89227290bb21bcf6b063adc8d5a96aa1f44a9bde
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2023-12-20 21:36:01 +0900

    Create CircleClass.cpp

---
+++ ch03_ClassAndObject/CircleClass.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Circle{
+public:
+  int radius;
+  double getArea(){ 
+    return 3.14 * radius * radius;
+  }
+};
+
+int main(){
+  Circle donut;
+  donut.radius = 3;
+  // donut 객체의 반지름을 3으로 설정
+  // public으로 선언되어 있으므로 가능
+  double area = donut.getArea();
+  cout << "도넛의 면적은 " << area << endl;
+
+  Circle pizza;
+  pizza.radius = 50;
+  // pizza 객체의 반지름을 50으로 설정
+  // public으로 선언되어 있으므로 가능
+  area = pizza.getArea();
+  cout << "피자의 면적은 " << area << endl;
+}
