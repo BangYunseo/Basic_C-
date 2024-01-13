74848b2cb07136351657928617045d1271654170
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-01-14 01:06:43 +0900

    Create Operator++.cpp

---
+++ ch10_OperatorOverloadingFunction/Operator++.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class Power{
+  int kick;
+  int punch;
+public:
+  Power(int kick = 0, int punch = 0){
+    this->kick = kick; this->punch = punch;
+  }
+  void show(){
+    cout << "Kick = " << kick << ',' << " Punch = " << punch << endl;
+  }
+  Power operator++(int x);
+  // 후위 ++ 연산자 함수 선언
+};
+
+// 전위 ++ 연산자 함수 구현
+Power Power::operator++(){
+  Power tmp = *this;
+  kick++;
+  punch++;
+  return tmp;
+  // 증가 이전 객체 상태 리턴
+}
+
+int main(){
+  Power a(3, 5), b;
+  a.show(); b.show();
+  
+  b = a++;
+  // 후위 ++ 연산자 사용
+  // operator++(int) 함수 호출
+  
+  a.show(); b.show();
+}
+
+// 출력 예시
+// Kick = 3, Punch = 5
+// Kick = 0, Punch = 0
+// Kick = 4, Punch = 6
+// Kick = 3, Punch = 5
+
+// a, b 출력한 후 b = a++ 후 a, b 출력
